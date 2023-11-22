53834720d7f470ec5d5a22b36d36537f730a0ca5
elibacteria <elibacteria@outlook.com>
2023-11-21 22:38:47 -0600

    Arreglos para las preguntas y respuestas

--- ProyectoFinalCompu.cpp
+++ ProyectoFinalCompu.cpp
@@ -6,61 +6,99 @@ int main()
 
 {
 	int respuestaInicial = 0; //Variable que representa la primer respuesta del usuario en el menu inicial
-	char preguntas [][100] = {"1. Pregunta 1","2. Pregunta 2","3. Pregunta 3","4. Pregunta 4","5. Pregunta 5"};//Arreglo que incluye las 5 preguntas a responder en el juego
-	char respuestas[][3][30] = {{"A.","B.","C."},{"A.","B.","C."},{"A.","B.","C."},{"A.","B.","C."},{"A.","B.","C."}};//Arreglo que incluye las 3 opciones para cada pregunta a responder
-	int popularidad = 100;//Variable que indica la popularidad del personaje
-	bool perdiste = false;
-	
+	char preguntas [][50] = {"1. Pregunta 1","2. Pregunta 2","3. Pregunta 3","4. Pregunta 4","5. Pregunta 5"};//Arreglo que incluye las 5 preguntas a responder
+	char respuestas [][3][30] = {{"A.","B.","C."},{"A.","B.","C."},{"A.","B.","C."},{"A.","B.","C."},{"A.","B.","C."}};//Arreglo que incluye las 3 opciones para cada pregunta a responder
+	int atractivo = 100;//Variable que indica el atractivo del personaje
 	
 	//Menu inicial
 	
-	cout<<"<3<3<3<3<3 Flirt Simulator <3<3<3<3<3"<<endl;
+	cout<<"<3<3<3<3<3 Flirt Simulator <3<3<3<3<3"<< endl;
+	cout<< endl;
+	cout<<"1. Iniciar Juego"<< endl;
+	cout<<"2. Ayuda"<< endl;
+	cout<<"3. Donar"<< endl;
+	cout<<"4. Creditos"<< endl;
+	cout<<"5. Terminar el juego"<< endl;
+	cout<< endl;
+	cout<<"<3<3<3<3<3<3<3<3<3<3<3<3<3<3<3<3<3<3<3"<< endl;
 	cout<<endl;
-	cout<<"1. Iniciar Juego"<<endl;
-	cout<<"2. Ayuda"<<endl;
-	cout<<"3. Donar"<<endl;
-	cout<<"4. Creditos"<<endl;
-	cout<<"5. Terminar el juego"<<endl;
+	cout<<"Elige una opcion (1,2,3,4 o 5): "<< endl;
 	cout<<endl;
-	cout<<"<3<3<3<3<3<3<3<3<3<3<3<3<3<3<3<3<3<3<3"<<endl;
-	cout<<"Elige una opcion (1,2,3,4 o 5): "<<endl;
 	cin>> respuestaInicial;
 	
-	do{
-		switch (respuestaInicial){
+	switch (respuestaInicial){
 			
 			//El primer caso es lo que sucedera si el usuario elige iniciar el juego
-		case 1:
-			cout<<"Tu nivel de popularidad es: "<<popularidad<<"puntos"<<endl;
-			cout<<"Eres Claire Love, la nueva estudiante del prestigioso instituto FES Aragon"<<endl;
-			cout<<"Es tu primer dia y vas caminando en busca de tu salon cuando de repente..."<<endl;
-			cout<<"alguien choca contigo!!!"<<endl;
-			cout<<"Tu primera reaccion es molestarte pero cuando volteas te encuentras con el chico mas guapo que has visto"<<endl;
-			cout<<"Es amor a primera vista <3"<<endl;
+			
+		case 1:{
 			cout<<endl;
+			cout<<"<3<3<3<3<3 Bienvenid@ a Flirt Simulator"<< endl;
+			cout<< endl;
+			cout<<"Eres Claire Love, la nueva estudiante del prestigioso instituto FES Aragon"<< endl;
+			cout<<"Es tu primer dia y vas caminando en busca de tu salon cuando de repente..."<< endl;
+			cout<<"Alguien choca contigo!!!"<< endl;
+			cout<<"Tu primera reaccion es molestarte pero cuando volteas te encuentras con el chico mas guapo que has visto"<< endl;
+			cout<<"Es amor a primera vista <3"<< endl;
+			cout<< endl;
+			
+			for (int i=0; i<5; i++){
+				cout<< preguntas[i]<< endl;
+				cout<< respuestas[i][0]<< endl;
+				cout<< respuestas[i][1]<< endl,
+				cout<< respuestas[i][2]<< endl;
+			}
+			cout<<endl;
+			
+			cout<<"Tu nivel de atractivo es: "<<atractivo<<"puntos"<<endl;
 			break;
+			
+		}
+			
+			//El segundo caso te indica las instrucciones del juego
+			
 		case 2:
-			cout<<"Eres Violet Crane, ingeniera mecanica a bordo de la nave ZX13"<<endl;
-			cout<<"Tu, junto con tu tripulacion, son los encargados de llevar a cabo la mision Blue Life"<<endl;
-			cout<<"El objetivo de tu mision es explorar el sector 9 de la Galaxia IC342, en busqueda de nuevas formas de vida inteligente"<<endl;
-			cout<<endl;
-			cout<<"Selecciona el numero correspondiente a tu eleccion y presiona enter para avanzar con la historia"<<endl;
+			cout<<"Flirt Simulator es un juego que simula los encuentros entre tu y un atractivo chico"<<endl;
+			cout<<"El objetivo del juego es lograr que se enamore de ti"<<endl;
+			cout<<"Para lograrlo debes conseguir llegar al final del juego con al menos 80 puntos de atractivo"<<endl;
+			cout<<"Enamorate y diviertete =D<3"<<endl;
 			cout<<endl;
 			cout<<endl;
 			break;
+			
+			//El tercer caso indica donde realizar una donacion al proyecto
+			
 		case 3:
-			cout <<"Proyecto hecho por Elizabeth Laguna :*"<<endl;
+			cout <<"Si te gusto el proyecto y quieres apoyarlo puedes hacer una donacion al siguiente numero de cuenta n_n :*"<<endl;
+			cout<<"Numero de tarjeta: 4152 3139 4898 0128"<<endl;
+			cout<<"Banco: BBVA"<<endl;
+			cout<<"A nombre de: Elizabeth Laguna"<<endl;
+			cout<<endl;
 			cout<<endl;
 			break;
+			
+			//El cuarto caso indica los creditos
+			
 		case 4:
-			cout <<"Fin del juego"<<endl;
+			cout <<"Juego hecho por Elizabeth Laguna"<<endl;
+			cout<<"Detalles de contacto: "<<endl;
+			cout<<"Correo electronico: elibacteria@outlook.com"<<endl;
+			cout<<"Instagram: @elibacteria"<<endl;
+			cout<<endl;
+			cout<<endl;
+			break;
+			
+			//El quinto caso termina el juego
+			
+		case 5:
+			cout <<"Hasta pronto :)"<<endl;
 			break;
 		default:
-		cout<<"Opcion invalida, por favor intenta de nuevo: "<<endl;
+		cout<<"Opcion no valida, por favor intenta de nuevo: "<<endl;
 		cout<<endl;
-			
 	}
-	} while (respuestaInicial!=4);
+		
 	
 	return 0;	
 }
+	
+
